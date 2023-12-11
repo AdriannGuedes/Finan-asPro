cc483a3175b4fd092e3023f3fa672e5e2aa0967b
Adriann Guedes <adrianguedes902@gmail.com>
2023-12-10 21:49:52 -0300

    primeiro commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import * as C from "./styles";
+
+const Header = () => {
+    return (
+        <C.Container>
+            <C.Header>
+                <C.Tittle>FinançasPro</C.Tittle>
+
+            </C.Header>
+        </C.Container>
+
+    )
+}
+
+export default Header;
